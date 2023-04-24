e7b59b9a2bb1c0c4997ba2553e4232ab7392ef7c
MunozCesarCM <munozcesar.cm@gmail.com>
2023-04-23 19:03:45 -0600

    Included the skills section

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
-const about = document.querySelector('#sample');
+const about = document.querySelector('#about');
+const skill = document.querySelector('#skills');
 
 window.addEventListener('scroll', () => {
+  const width = document.body.getBoundingClientRect().width;
   let offsetY = window.scrollY;
 
-  about.style.transform = `translateX(${offsetY * 1.1 - 350}px)`;
+  about.style.transform = `translateX(${offsetY * 0.6 - 200}px)`;
+  skill.style.transform = `translateX(${(width * 0.6) - offsetY * 0.6 + 200}px)`;
 });
