46abefb4b7fc239af46970095c810bf927073e77
MunozCesarCM <munozcesar.cm@gmail.com>
2023-04-29 10:00:51 -0600

    Refactor

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 const about = document.querySelector('#about');
 const skill = document.querySelector('#skills');
+const scroll = document.querySelector('.scroll');
 
 window.addEventListener('scroll', () => {
   const width = document.body.getBoundingClientRect().width;
   let offsetY = window.scrollY;
 
   about.style.transform = `translateX(${offsetY * 0.6 - 200}px)`;
-  skill.style.transform = `translateX(${(width * 0.6) - offsetY * 0.6 + 200}px)`;
+  skill.style.transform = `translateX(${width - offsetY * 0.6}px)`;
+  scroll.style.opacity = 1.25 - offsetY / 450;
 });
 
 // Scroll to top on window reload
