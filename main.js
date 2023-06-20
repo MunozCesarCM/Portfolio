dc734d60a5d782e8b08bc2e1da8ef6883f865625
MunozCesarCM <munozcesar.cm@gmail.com>
2023-06-20 10:29:05 -0600

    Created shapes and movement

--- main.js
+++ main.js
@@ -1,38 +1,72 @@
-const about = document.querySelector('#about');
-const skill = document.querySelector('#skills');
-const scroll = document.querySelector('.scroll');
+const body = document.querySelector('html');
+const yellow_circle = document.querySelector('#yellow-circle');
+const black_circle = document.querySelector('#black-circle');
+const black_outline = document.querySelector('#black-outline');
 
-const containers = document.querySelectorAll('.screenshots')
-const config = [
-  { pos: 15, speed: 13, },
-  { pos: 35, speed: 10, },
-  { pos: 70, speed: 15, },
-  { pos: 00, speed: 09, },
-];
+const onScroll = () => {
+  if (window.pageYOffset === 0) {
+    yellow_circle.style.transform = `translateX(0px) translateY(0px)`;
+    yellow_circle.style.width = `80vw`;
+    yellow_circle.style.height = `80vw`;
 
-window.addEventListener('scroll', () => {
-  const width = document.body.getBoundingClientRect().width;
-  const offsetY = window.scrollY;
-  const skillPos = skill.getBoundingClientRect().bottom;
+    black_circle.style.transform = `translateX(0px) translateY(0px)`;
+    black_circle.style.top = '33vw';
+    black_circle.style.right = '70vw';
+    black_circle.style.width = `50vw`;
+    black_circle.style.height = `50vw`;
+    black_circle.style.backgroundColor = '#2F2F2F';
 
-  about.style.transform = `translateX(${offsetY * 0.6 - 200}px)`;
-  skill.style.transform = `translateX(${skillPos - (width * 0.1)}px)`;
-  scroll.style.opacity = 1.25 - offsetY / 450;
+    black_outline.style.transform = `translateX(0px) translateY(0px)`;
+    black_outline.style.top = '40vw';
+    black_outline.style.right = '45vw';
+    black_outline.style.width = `60vw`;
+    black_outline.style.height = `60vw`;
+    black_outline.style.borderColor = '#2F2F2F';
 
-  containers.forEach((container) => {
-    const top = container.getBoundingClientRect().top;
-    const images = container.querySelectorAll('img');
-    const height = container.offsetHeight;
+    body.style.backgroundColor = '#FFFFFF';
+  }
+  if (window.pageYOffset > (window.innerHeight - 10)) {
+    yellow_circle.style.transform = `translateX(-58vw) translateY(-50vw)`;
+    yellow_circle.style.width = `120vw`;
+    yellow_circle.style.height = `120vw`;
 
-    images.forEach((image, index) => {
-      const { pos, speed } = config[index];
-      const position = top * (speed / 10) - (height * pos / 100) - 200;
-      image.style.transform = `translateY(${position}px)`;
-    })
-  });
-});
+    black_circle.style.transform = `translateX(46vw) translateY(5vw)`;
+    black_circle.style.top = '33vw';
+    black_circle.style.right = '70vw';
+    black_circle.style.width = `50vw`;
+    black_circle.style.height = `50vw`;
+    black_circle.style.backgroundColor = '#2F2F2F';
 
-// Scroll to top on window reload
-window.onbeforeunload = function () {
-  window.scrollTo(0, 0);
-}
+    black_outline.style.transform = `translateX(13vw) translateY(3vw)`;
+    black_outline.style.top = '40vw';
+    black_outline.style.right = '45vw';
+    black_outline.style.width = `60vw`;
+    black_outline.style.height = `60vw`;
+    black_outline.style.borderColor = '#2F2F2F';
+
+    body.style.backgroundColor = '#FFFFFF';
+  }
+  if (window.pageYOffset > (window.innerHeight * 2 - 10)) {
+    yellow_circle.style.transform = `translateX(0vw) translateY(-100vw)`;
+    yellow_circle.style.width = `20vw`;
+    yellow_circle.style.height = `20vw`;
+
+    black_circle.style.transform = `translateX(0vw) translateY(0vw)`;
+    black_circle.style.top = '-4vw';
+    black_circle.style.right = '45vw';
+    black_circle.style.width = `10vw`;
+    black_circle.style.height = `10vw`;
+    black_circle.style.backgroundColor = '#FFFFFF';
+
+    black_outline.style.transform = `translateX(0vw) translateY(0vw)`;
+    black_outline.style.top = '-40vw';
+    black_outline.style.right = '5vw';
+    black_outline.style.width = `90vw`;
+    black_outline.style.height = `90vw`;
+    black_outline.style.borderColor = '#FFFFFF';
+
+    body.style.backgroundColor = '#007FFF';
+  }
+};
+
+window.addEventListener('scroll', onScroll);
